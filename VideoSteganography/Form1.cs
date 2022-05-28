fc47da1f8eb17daea55bb2388f27093abc44da01
huseyinmertkok <huseyinmert.koek@gmail.com>
2022-05-28 17:55:03 +0300

    UI Design

--- VideoSteganography/Form1.cs
+++ VideoSteganography/Form1.cs
@@ -15,6 +15,39 @@ namespace VideoSteganography
         public Form1()
         {
             InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel1.BackColor = Color.FromArgb(100, 0, 0, 0);
+            panel2.BackColor = Color.FromArgb(100, 0, 0, 0);
+            panel3.BackColor = Color.FromArgb(100, 0, 0, 0);
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
---
+++ VideoSteganography/Stegano.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoSteganography
+{
+    internal class Stegano
+    {
+        private string coverText;
+        private string coveredMessage;
+
+        public Stegano() 
+        { 
+        }
+
+        //------------------------------------------------------GETTERS & SETTERS------------------------------------------------------
+
+        //cover text
+        public void setCoverText(string coverText)
+        {
+            this.coverText = coverText;
+        }
+
+        public string getCoverText()
+        {
+            return this.coverText;
+        }
+
+        //coveredMessage
+        public void setCoveredMessage(string coveredMessage)
+        {
+            this.coveredMessage = coveredMessage;
+        }
+
+        public string getCoveredMessage()
+        {
+            return this.coveredMessage;
+        }
+    }
+}
