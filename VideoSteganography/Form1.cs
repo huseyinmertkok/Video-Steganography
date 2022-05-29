dd376f34c14157c0cb8a5bb7c0eed86ee9e085fb
huseyinmertkok <huseyinmert.koek@gmail.com>
2022-05-29 18:05:57 +0300

    Video Implementation

--- VideoSteganography/Form1.cs
+++ VideoSteganography/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,27 +13,21 @@ namespace VideoSteganography
 {
     public partial class Form1 : Form
     {
+        VideoFile coverVideo;
+        VideoFile newVideo;
+
         public Form1()
         {
             InitializeComponent();
             StartPosition = FormStartPosition.CenterScreen;
         }
 
-        private void panel3_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             panel1.BackColor = Color.FromArgb(100, 0, 0, 0);
             panel2.BackColor = Color.FromArgb(100, 0, 0, 0);
             panel3.BackColor = Color.FromArgb(100, 0, 0, 0);
-        }
-
-        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
-        {
-
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -42,12 +37,24 @@ namespace VideoSteganography
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            using (OpenFileDialog ofd = new OpenFileDialog()
+            {
+                Multiselect = false, ValidateNames = true, Filter = "All Video Files|*.wmv;*.wav;*.mp3;*.mp4;*.mkv;*.avi"
+            })
+            {
+                if(ofd.ShowDialog() == DialogResult.OK)
+                {
+                    FileInfo fi= new FileInfo(ofd.FileName);
+                    coverVideo = new VideoFile(fi.FullName, Path.GetFileName(fi.FullName));
+                    label7.Text = Path.GetFileName(fi.FullName) + " <- Ready";
+                }
+            }
         }
 
-        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        private void button4_Click(object sender, EventArgs e)
         {
-
+            //File.Copy( Path.GetFullPath(coverVideo.getPath()), @"D:\Workspace\asp_net\test\test.mp4");
+            newVideo = new VideoFile(coverVideo.getFrameList(), coverVideo.getWidth(), coverVideo.getHeight(), coverVideo.getFilename());
         }
     }
 }
